44bcb674b4c4b1fbaa1317a448aff3662f025424
Gage <gagemigan@gmail.com>
2020-06-02 13:29:30 -0400

    added solve, we kinda had it

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // eslint-disable-next-line strict
 $('.thumbnail').on('click', e => {
-  $('.hero img').attr('src', e.target);
+  // this is what we had before the solve,
+  //$('.hero img').attr('src', e.target);
+
+  const imgSrc = $(e.currentTarget).find('img').attr('src');  
+  const imgAlt = $(e.currentTarget).find('img').attr('alt');
+ 
+ $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
 });
